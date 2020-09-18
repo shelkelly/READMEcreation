d818a76fc7afa4621fdc4ede2242208cfe58066a
Shelby <shelbypkelly@gmail.com>
2020-09-18 00:50:18 -0400

    updating node funct

--- index.js
+++ index.js
@@ -4,41 +4,50 @@ const questions = [{
     name: "title",
     message: "Title: "
 },
-{ type: "input",
-name: "description",
-message: "Description: "
- },
- {type: "input",
- name: "tableoc",
- message: "Table of Contents: "
+{
+    type: "input",
+    name: "description",
+    message: "Description: "
 },
-{type: "input",
-name: "installation",
-message: "Installation: "
+{
+    type: "input",
+    name: "tableoc",
+    message: "Table of Contents: "
 },
-{type: "input",
-name: "usage",
-message: "Usage: "
+{
+    type: "input",
+    name: "installation",
+    message: "Installation: "
 },
-{type: "input",
-name: "license",
-message: "License: "
+{
+    type: "input",
+    name: "usage",
+    message: "Usage: "
 },
-{type: "input",
-name: "contributing",
-message: "Contributing: "
+{
+    type: "input",
+    name: "license",
+    message: "License: "
 },
-{type: "input",
-name: "tests",
-message: "Tests: "
+{
+    type: "input",
+    name: "contributing",
+    message: "Contributing: "
 },
-{type: "input",
-name: "questions",
-message: "Questions: "
+{
+    type: "input",
+    name: "tests",
+    message: "Tests: "
+},
+{
+    type: "input",
+    name: "questions",
+    message: "Questions: "
 }];
 
 const inquirer = require("inquirer");
 var fs = require("fs");
+const genmarkjs= require("./utils/generateMarkdown");
 
 
 
@@ -46,10 +55,17 @@ function promptUser() {
     return inquirer.prompt(questions);
 }
 
-// function to write README file
-function writeToFile(fileName, data) {
 
-}
+// function to write README file
+fs.writeFile("README.md", generateMarkdown, function(err) {
+
+    if (err) {
+      return console.log(err);
+    }
+  
+  });
+
+
 
 // function to initialize program
 function init() {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,14 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
+  ${data.description}
+  ${data.tableoc}
+  ${data.installation}
+  ${data.usage}
+  ${data.license}
+  ${data.contributing}
+  ${data.tests}
+  ${data.questions}`;
 }
 
 module.exports = generateMarkdown;
