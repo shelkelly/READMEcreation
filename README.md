4150079bfcf587ab392cb57ad74aa3000cfe68b4
Shelby <shelbypkelly@gmail.com>
2020-09-19 21:31:24 -0400

    generateMarkdown updates. getting undefined with generation, no error.

---
+++ README.md
@@ -0,0 +1,25 @@
+# undefined
+
+    ## Description
+    undefined
+
+    ##Table of Contents
+    # undefined
+
+    ## Installation Instructions
+    undefined
+
+    ## Usage Information
+    undefined
+
+    ## License
+    undefined
+
+    ## Contribution Guidelines
+    undefined
+
+    ## undefined
+
+    ## Questions?
+    ### Email: ### undefined
+    ### GitHub: [undefined](github.com/shelkelly).
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const inquirer = require("inquirer");
+var fs = require("fs");
+const util = require("util");
+
 // array of questions for user
 const questions = [{
     type: "input:",
@@ -25,9 +29,10 @@ const questions = [{
     message: "Usage: "
 },
 {
-    type: "input",
+    type: "checkbox",
     name: "license",
-    message: "License: "
+    message: "License: ",
+    choices: [ "MIT", new inquirer.Separator(), "LGPL", new inquirer.Separator(), "MPL", new inquirer.Separator(), "AGPL", new inquirer.Separator, "Apache", new inquirer.Separator, "GPL", new inquirer.Separator(), "Unlicense" ]
 },
 {
     type: "input",
@@ -41,37 +46,57 @@ const questions = [{
 },
 {
     type: "input",
-    name: "questions",
-    message: "Questions: "
+    name: "github",
+    message: "GitHub Username: "
+},
+{
+type: "input",
+name: "email",
+message: "Email: "
 }];
 
-const inquirer = require("inquirer");
-var fs = require("fs");
-const genmarkjs= require("./utils/generateMarkdown");
+const writeFileAsync = util.promisify(fs.writeFile);
 
+    function promptUser() {
+        return inquirer.prompt(questions);
+    };
 
+    function generateMarkdown(answers) {
+        return `# ${questions.title}
 
-function promptUser() {
-    return inquirer.prompt(questions);
-}
+    ## Description
+    ${questions.description}
 
+    ##Table of Contents
+    # ${questions.tableoc}
 
-// function to write README file
-fs.writeFile("README.md", generateMarkdown(), function(err) {
+    ## Installation Instructions
+    ${questions.installation}
 
-    if (err) {
-      return console.log(err);
+    ## Usage Information
+    ${questions.usage}
+
+    ## License
+    ${questions.license}
+
+    ## Contribution Guidelines
+    ${questions.contributing}
+
+    ## ${questions.tests}
+
+    ## Questions?
+    ### Email: ### ${questions.email}
+    ### GitHub: [${questions.github}](github.com/shelkelly).`;
     }
-  
-  });
 
+    promptUser()
+        .then(function (answers) {
+            const genMark = generateMarkdown(answers);
 
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
-promptUser();
+            return writeFileAsync("README.md", genMark);
+        }).then(function () {
+            console.log("Successfully wrote to README.md")
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
